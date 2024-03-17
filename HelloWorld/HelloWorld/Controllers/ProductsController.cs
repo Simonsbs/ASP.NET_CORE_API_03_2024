24027a2ed8b1ffbbc3495e6db0d822f441e74d2c
Simon B.Stirling <simonsbs@gmail.com>
2024-03-17 12:15:41 +0200

    Update ProductsController.cs
    .

--- HelloWorld/HelloWorld/Controllers/ProductsController.cs
+++ HelloWorld/HelloWorld/Controllers/ProductsController.cs
@@ -1,9 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HelloWorld.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HelloWorld.Controllers;
 
 [ApiController]
-[Route("api/categories/{catergoryID}/products")]
+[Route("api/categories/{categoryID}/products")]
 public class ProductsController : ControllerBase {
+    [HttpGet]
+    public ActionResult<List<ProductDTO>> GetProducts(int categoryID) {
+        if (CategoryExists(categoryID, out CategoryDTO category)) { 
+            return NotFound();
+        }
+        return Ok(category.Products);
+    }
 
+    [HttpGet("{productID}")]
+    public ActionResult<ProductDTO> GetProduct(int categoryID, int productID) {
+        if (CategoryExists(categoryID, out CategoryDTO category)) {
+            return NotFound();
+        }
+        ProductDTO product = category.Products.FirstOrDefault(p => p.ID == productID);
+        if (product == null) {
+            return NotFound();
+        }
+        
+        return Ok(product);
+    }
+
+    private bool CategoryExists(int categoryID, out CategoryDTO category) {
+        category = MyDataStore.Categories.FirstOrDefault(c => c.ID == categoryID);
+        return category == null;
+    }
 }
